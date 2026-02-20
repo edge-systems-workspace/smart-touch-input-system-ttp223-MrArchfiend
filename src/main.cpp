fdb502bb9ca87d15bdb2cb79f538a31eb14bb4ec
Aditya Singh <Aditya.singh_cs.aiml24@gla.ac.in>
2026-02-20 08:52:35 +0530

    Read digital value from touch sensor

--- src/main.cpp
+++ src/main.cpp
@@ -18,20 +18,23 @@ int touchState = LOW;
 
 void setup() {
 
-    // TODO 3:
-    // Initialize Serial communication (9600 baud rate)
+    // TODO 3: Initialize Serial communication (9600 baud rate)
+    Serial.begin(9600);
 
-    // TODO 4:
-    // Configure touch pin as INPUT
+    // TODO 4: Configure touch pin as INPUT
+    pinMode(TOUCH_PIN, INPUT);
 
-    // TODO 5:
-    // Print system initialization message
+    // TODO 5: Print system initialization message
+    Serial.println("======================================");
+    Serial.println(" Embedded Touch Detection System ");
+    Serial.println("======================================");
+    Serial.println("Monitoring TTP223 sensor on Pin 2...");
 }
 
 void loop() {
 
-    // TODO 6:
-    // Read digital value from touch sensor
+    // TODO 6: Read digital value from touch sensor
+    touchState = digitalRead(TOUCH_PIN);
 
     // TODO 7:
     // If touch detected (HIGH)
